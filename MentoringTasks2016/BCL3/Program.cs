aab46f50c80820c6e9a0ad5a83436b602d73e5c0
Alexandr Cherkas <aleksandr_cherkas@epam.com>
2016-08-08 21:07:39 +0300

    Implemented BCL3

---
+++ MentoringTasks2016/BCL3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace BCL3
+{
+    internal static class Concatenator
+    {
+        public static string SpecificConcatenate(string[] strings)
+        {
+            var symbolsCount = 0;
+            for (int i = 0; i < strings.Length; i += 2)
+                symbolsCount += strings[i].Length;
+
+            var builder = new StringBuilder(symbolsCount);
+            for (int i = 0; i < strings.Length; i += 2)
+                builder.AppendLine(strings[i]);
+
+            return builder.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var strings = new string[short.MaxValue];
+            for (int i = 0; i < strings.Length; i++)
+            {
+                strings[i] = i.ToString();
+            }
+
+            var concatenatedString = Concatenator.SpecificConcatenate(strings);
+            Console.WriteLine(concatenatedString);
+            Console.ReadLine();
+        }
+    }
+}
