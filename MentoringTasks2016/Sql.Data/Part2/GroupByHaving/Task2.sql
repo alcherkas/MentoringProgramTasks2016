d26c7b07328935da8fc0fbea5d6661dfa2aefecb
Alexandr Cherkas <aleksandr_cherkas@epam.com>
2016-09-05 21:04:53 +0300

    fixes

--- MentoringTasks2016/Sql.Data/Part2/GroupByHaving/Task2.sql
+++ MentoringTasks2016/Sql.Data/Part2/GroupByHaving/Task2.sql
@@ -1,1 +1,5 @@
-﻿
+﻿select (select Concat(LastName, ' ', FirstName) from Employees where Employees.EmployeeID = Orders.EmployeeID) as 'Seller',
+count(*) as 'Amount'
+from Orders
+group by EmployeeID
+order by Amount desc
