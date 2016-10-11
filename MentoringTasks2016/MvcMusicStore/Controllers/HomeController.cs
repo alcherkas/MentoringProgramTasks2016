84597f9bc9f556acf98af281d94f1a9bdad30b0d
Alexandr Cherkas <aleksandr_cherkas@epam.com>
2016-10-11 21:14:59 +0300

    Logging

---
+++ MentoringTasks2016/MvcMusicStore/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+using PerformanceMetrics;
+
+namespace MvcMusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly MusicStoreEntities _storeContext = new MusicStoreEntities();
+        private readonly Counters _performanceCounters = new Counters();
+
+        // GET: /Home/
+        public async Task<ActionResult> Index()
+        {
+            _performanceCounters.IncrementHome();
+            return View(await _storeContext.Albums
+                .OrderByDescending(a => a.OrderDetails.Count())
+                .Take(6)
+                .ToListAsync());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _storeContext.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MentoringTasks2016/MvcMusicStore/{basedir}/Sample1.cmd
@@ -0,0 +1,1 @@
+"%ProgramFiles(x86)%\Log Parser 2.2\LogParser.exe" -i:CSV "SELECT Level, COUNT(*) FROM '.\logs\2016-10-11.txt' group by level "
